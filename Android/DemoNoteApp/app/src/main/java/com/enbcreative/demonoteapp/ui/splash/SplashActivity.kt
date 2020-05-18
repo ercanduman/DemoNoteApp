fe1cccccb3d962aedc8718b27a8e9fff066d42ac
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-05-18 14:33:59 +0300

    SplashActivity.kt finished

--- Android/DemoNoteApp/app/src/main/java/com/enbcreative/demonoteapp/ui/splash/SplashActivity.kt
+++ Android/DemoNoteApp/app/src/main/java/com/enbcreative/demonoteapp/ui/splash/SplashActivity.kt
@@ -1,12 +1,13 @@
-package com.enbcreative.demonoteapp
+package com.enbcreative.demonoteapp.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enbcreative.demonoteapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
