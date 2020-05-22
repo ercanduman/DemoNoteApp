6dec06a05c1880954050ab9aa5c490a9ac8428ed
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-05-22 23:55:56 +0300

    NoteDataSource.kt added to add sample notes to local database

--- Android/DemoNoteApp/app/src/main/java/com/enbcreative/demonoteapp/data/repository/NoteRepository.kt
+++ Android/DemoNoteApp/app/src/main/java/com/enbcreative/demonoteapp/data/repository/NoteRepository.kt
@@ -1,5 +1,7 @@
 package com.enbcreative.demonoteapp.data.repository
 
+import androidx.lifecycle.LiveData
+import com.enbcreative.demonoteapp.data.backup.data.NoteDataSource
 import com.enbcreative.demonoteapp.data.db.AppDatabase
 import com.enbcreative.demonoteapp.data.db.model.note.Note
 
@@ -8,5 +10,12 @@ class NoteRepository(private val db: AppDatabase) {
     suspend fun updateNote(note: Note) = db.getNoteDao().update(note)
     suspend fun deleteNote(note: Note) = db.getNoteDao().delete(note)
     suspend fun getNoteCount() = db.getNoteDao().getNoteCount()
-    suspend fun getAllNotes() = db.getNoteDao().getAllNotes()
+    suspend fun getAllNotes(): LiveData<List<Note>> {
+        val noteCount = db.getNoteDao().getNoteCount()
+        if (noteCount == 0) {
+            val notes = NoteDataSource().getNotes()
+            notes.forEach { db.getNoteDao().insert(it) }
+        }
+        return db.getNoteDao().getAllNotes()
+    }
 }
